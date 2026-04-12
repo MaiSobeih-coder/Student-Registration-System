7297bb2f788df6c08b1ceed4814669a24f48c85f
Mai <123235591+MaiSobeih-coder@users.noreply.github.com>
2026-04-12 18:14:40 +0300

    Initial commit

---
+++ src/k/Student.java
@@ -0,0 +1,70 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package k;
+
+
+import java.io.Serializable;
+import java.time.Year;
+import java.util.ArrayList;
+
+
+
+/**
+ *
+ * @author aya
+ */
+public class Student implements Serializable{
+    private String id;
+    private String name;
+    private String gender;
+    private ArrayList<String> pl =new ArrayList<>();
+    static int counter;
+    public Student(String name, String gender) {
+        this.name = name;
+        this.gender = gender;
+        String std_id = this.gender.equals("male")?"1":"2";
+        this.id =  std_id+Year.now().getValue()+String.format("%04d", counter++); 
+    }
+
+
+    
+    public String getId() {
+        return id;
+    }
+
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public ArrayList<String> getPl() {
+        return pl;
+    }
+
+    public void setPl(ArrayList<String> pl) {
+        this.pl = pl;
+    }
+
+    @Override
+    public String toString() {
+        return this.name+" - "+this.id;
+    }
+    
+    
+    
+}
